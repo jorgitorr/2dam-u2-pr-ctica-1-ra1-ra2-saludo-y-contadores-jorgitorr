1df6aa01f7e6565cf0a5b203b3425f638f91c87b
jorgitorr <jorgitorr97@gmail.com>
2023-11-15 11:23:01 +0100

    introduce datos en la barra de dialogo, dialogo checked

--- app/src/main/java/com/jorgearceruiz97/a2dam_u2_pr_ctica_1_ra1_ra2_saludo_y_contadores_jorgitorr/MainActivity.kt
+++ app/src/main/java/com/jorgearceruiz97/a2dam_u2_pr_ctica_1_ra1_ra2_saludo_y_contadores_jorgitorr/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,7 +20,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.jorgearceruiz97.a2dam_u2_pr_ctica_1_ra1_ra2_saludo_y_contadores_jorgitorr.ui.theme._2damu2prctica1ra1ra2saludoycontadoresjorgitorrTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,18 +47,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    dialogo()
+                    pantallaBoton()
                 }
             }
         }
     }
 }
 
-
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun pantallaBoton(nombre: String, cont:Int){
-    val texto:String = ""
+fun pantallaBoton(){
+    var nombre by rememberSaveable { mutableStateOf("") }
+    var contAceptar by rememberSaveable { mutableStateOf(0) }
+    var contCancelar by rememberSaveable { mutableStateOf(0) }
+    var showDialogo by rememberSaveable { mutableStateOf(false) }
+
+
+    if(showDialogo){
+        dialogo(
+            onDissmissRequest = {showDialogo = false} ,
+            onAccept = {contAceptar++},
+            accValue = {contAceptar.toString()},
+            onCancel = {contCancelar++},
+            canValue = {contCancelar.toString()},
+            onTextChange = {it : String -> nombre = it},
+            nameValue = {nombre}
+        )
+    }
+
 
     Column(
         Modifier.fillMaxSize(),
@@ -65,11 +82,11 @@ fun pantallaBoton(nombre: String, cont:Int){
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Button(modifier = Modifier.padding(bottom = 10.dp), onClick = {
-
+            showDialogo = true
         } ) {
             Text(text = "Saludar")
         }
-        Text(text = texto,
+        Text(text = "Saludos $nombre",
             fontSize = 20.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier
@@ -80,14 +97,17 @@ fun pantallaBoton(nombre: String, cont:Int){
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
-@Composable
-fun dialogo(){
-    var contAceptar by rememberSaveable { mutableStateOf(0) }
-    var contCancelar by rememberSaveable { mutableStateOf(0) }
-    var text by rememberSaveable { mutableStateOf("") }
 
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview(showBackground = true)
+@Composable
+fun dialogo(onDissmissRequest: () -> Unit,//está funcion se llama cuando el usuario da fuera de la ventana
+            onAccept: () -> Unit,
+            accValue: () -> String,
+            onCancel: () -> Unit,
+            canValue: () -> String,
+            onTextChange: (String) -> Unit,
+            nameValue: () -> String){
     Column(
         Modifier
             .fillMaxWidth()
@@ -95,35 +115,57 @@ fun dialogo(){
             .padding(top = 15.dp, bottom = 20.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
-        Text(text = "Configuracion",
-            Modifier.padding(bottom = 2.dp, start = 250.dp), fontSize = 20.sp)
 
-        OutlinedTextField(
-            value = text,
-            onValueChange = { text = it},
-            label = { Text(text = "Introduce tu nombre")},
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.White,
-                textColor = Color.Black))
+        Dialog(onDismissRequest = { onDissmissRequest() },
+            properties = DialogProperties(dismissOnBackPress = true)
+        ) {
+            Row(){
+                Text(text = "Configuracion",
+                    Modifier.padding(bottom = 2.dp, start = 250.dp), fontSize = 20.sp)
+            }
 
-        Row (
-            Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center) {
-            Button(onClick = { contAceptar++ }) {
-                Text(text = "Aceptar")
+            Row(Modifier.fillMaxWidth()){
+                OutlinedTextField(
+                    value = nameValue(),
+                    onValueChange = { onTextChange(it) },
+                    label = { Text(text = "Introduce tu nombre") },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        textColor = Color.White
+                    )
+                )
             }
-            Button(onClick = { text = "" }, enabled = text.isNotEmpty()) {
-                Text(text = "Limpiar")
-            }
-            Button(onClick = { contCancelar++ }) {
-                Text(text = "Cancelar")
+
+            Row (
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 15.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = {
+                    onAccept()
+                    onDissmissRequest()//sale
+                }) {
+                    Text(text = "Aceptar ${accValue()}")
+                }
+                Button(onClick = { onTextChange("") }, enabled = nameValue().isNotEmpty()) {
+                    Text(text = "Limpiar")
+                }
+                Button(onClick = {
+                    onCancel()
+                    onDissmissRequest()//sale
+                    onTextChange("")
+
+                }) {
+                    Text(text = "Cancelar ${canValue()}")
+                }
             }
         }
 
+
+
     }
+
 
 }
